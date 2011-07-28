0fd784f4a171fb074f736cd92e786a1543d628c2
Axel Hecht <axel@pike.org>
2011-07-28 18:03:06 +0200

    mostly pep8, but also more tests

--- django_durationRel/tests/models.py
+++ django_durationRel/tests/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django_durationRel.fields import DurationRelField
 
+
 class Other(models.Model):
     code = models.CharField(max_length=128)
+
 
 class One(models.Model):
     code = models.CharField(max_length=128)
--- django_durationRel/tests/tests.py
+++ django_durationRel/tests/tests.py
@@ -11,16 +11,34 @@ class OneOtherTest(test.TestCase):
         self.one = One.objects.create(code="one")
         self.other = Other.objects.create(code="other")
         One.others.through.objects.create(one=self.one, other=self.other)
-        
+
     def test_basic_relation(self):
         """
         Tests that there is exactly one other
         """
         self.assertEqual(self.one.others.count(), 1)
-        self.assertEqual(self.one.durationRel_others.filter(enddate__isnull=False).count(), 0)
+        self.assertEqual((self.one
+                          .durationRel_others
+                          .filter(enddate__isnull=False).count()),
+                          0)
         self.assertEqual(self.one.others.all()[0].code, "other")
+
     def test_current(self):
         """
         Tests that get_current_others works.
         """
         self.assertEqual(list(self.one.get_current_others()), [self.other])
+
+    def test_serialization(self):
+        """
+        Tests that we can serialize and deserialize.
+        """
+        from django.core import serializers
+        from django.core.management.commands import dumpdata
+        out = dumpdata.Command().handle("tests")
+        objects = dict((o.object._meta.object_name, o.object)
+                       for o in serializers.deserialize("json", out))
+        self.assertEqual(self.one, objects['One'])
+        self.assertEqual(self.other, objects['Other'])
+        self.assertEqual(self.one, objects["One_others"].one)
+        self.assertEqual(self.other, objects["One_others"].other)
